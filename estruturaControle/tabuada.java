3cdee653a1b036fa35824357b3c1de5ee82d3369
“cissamil“ <clarissamorita1@gmail.com>
2025-11-18 00:00:41 -0300

    feat: tabuada.java

--- estruturaControle/tabuada.java
+++ estruturaControle/tabuada.java
@@ -1,9 +1,19 @@
 
   // Escreva um código onde o usuário entra com um número e seja gerada a tabuada de 1 até 10 desse número;
 package estruturaControle;
+import java.util.Scanner;
 
 public class tabuada{
-
+  public static void main(String args[]){
+    var scanner = new Scanner(System.in);
+    System.out.printf("Dgite um numero e mostrarei a tabuada dele:");
+    int num = scanner.nextInt();
+    for(int fator = 0; fator < 11; fator++){
+      int tabuada = num * fator;
+      System.out.printf("Tabuada do %d: %d\n", num, tabuada);
+    }
+    
+  }
  
 }
     
